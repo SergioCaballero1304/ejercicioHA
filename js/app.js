2314b7cb7c87ff1a0da13806c5fd23ac17801343
Sergio Caballero <sergiocaballero1304@gmail.com>
2024-10-16 20:45:05 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const botonPelicula = document.querySelector("#botonPelicula");
+const url = "https://private.omdbapi.com/?apikey=bef9c583&t=";
+
+botonPelicula.addEventListener("click", function () {
+  const inputPelicula = document.querySelector("input").value;
+  fetch(url + inputPelicula)
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    .then(function (infoPelicula) {
+      document.querySelector("#ocultar").style.display = "block";
+      document.querySelector("#principal").style.display = "none";
+      // const titulo = document.querySelector("title");
+      // titulo.textContent = infoPelicula.Title;
+      document.querySelector("#title").textContent = infoPelicula.Title;
+      document.querySelector("#director").textContent =
+        "Director: " + infoPelicula.Director;
+      document.querySelector("#plot").textContent = infoPelicula.Plot;
+      document.querySelector("#actors").textContent = infoPelicula.Actors;
+      document.querySelector("#country").textContent = infoPelicula.Country;
+      document.querySelector("#awards").textContent = infoPelicula.Awards;
+
+      const listaRatings = document.querySelector("ul");
+      let liRating = "";
+      for (const rating of infoPelicula.Ratings) {
+        liRating += "<li>" + rating.Source + " - " + rating.Value + "</li>";
+      }
+      listaRatings.innerHTML = "";
+      listaRatings.insertAdjacentHTML("beforeend", `${liRating}`);
+      const imagen = document.querySelector("#imagen");
+      imagen.innerHTML = "";
+      imagen.insertAdjacentHTML(
+        "beforeend",
+        `<img src="${infoPelicula.Poster}" alt="Foto">`
+      );
+    });
+});
