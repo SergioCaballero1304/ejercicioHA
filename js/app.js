54985d0f4f85e83ae760e6fa58472a6adfc245ef
Sergio Caballero <sergiocaballero1304@gmail.com>
2024-10-17 01:14:02 -0300

    finish

--- js/app.js
+++ js/app.js
@@ -8,28 +8,51 @@ botonPelicula.addEventListener("click", function () {
       return respuesta.json();
     })
     .then(function (infoPelicula) {
-      document.querySelector("#ocultar").style.display = "block";
-      document.querySelector("#principal").style.display = "none";
-      document.querySelector("#title").textContent = infoPelicula.Title;
-      document.querySelector("#director").textContent =
-        "Director: " + infoPelicula.Director;
-      document.querySelector("#plot").textContent = infoPelicula.Plot;
-      document.querySelector("#actors").textContent = infoPelicula.Actors;
-      document.querySelector("#country").textContent = infoPelicula.Country;
-      document.querySelector("#awards").textContent = infoPelicula.Awards;
+      if (infoPelicula.Response === "False") {
+        document.querySelector("#ocultar").style.display = "none";
+        document.querySelector("#principal").style.display = "none";
+        document.querySelector("#error").style.display = "block";
+        imagen.innerHTML = "";
+      } else {
+        document.querySelector("#error").style.display = "none";
+        document.querySelector("#ocultar").style.display = "block";
+        document.querySelector("#principal").style.display = "none";
+        document.querySelector("#title").textContent = infoPelicula.Title;
+        document.querySelector("#director").textContent =
+          "Director: " + infoPelicula.Director;
+        document.querySelector("#plot").textContent = infoPelicula.Plot;
+        document.querySelector("#actors").textContent = infoPelicula.Actors;
+        document.querySelector("#country").textContent = infoPelicula.Country;
+        document.querySelector("#awards").textContent = infoPelicula.Awards;
 
-      const listaRatings = document.querySelector("ul");
-      let liRating = "";
-      for (const rating of infoPelicula.Ratings) {
-        liRating += "<li>" + rating.Source + " - " + rating.Value + "</li>";
+        const listaRatings = document.querySelector("ul");
+        let liRating = "";
+        for (const rating of infoPelicula.Ratings) {
+          liRating += "<li>" + rating.Source + " - " + rating.Value + "</li>";
+        }
+        listaRatings.innerHTML = "";
+        listaRatings.insertAdjacentHTML("beforeend", `${liRating}`);
+        const imagen = document.querySelector("#imagen");
+        imagen.innerHTML = "";
+        imagen.insertAdjacentHTML(
+          "beforeend",
+          `<img src="${infoPelicula.Poster}" alt="Foto">`
+        );
       }
-      listaRatings.innerHTML = "";
-      listaRatings.insertAdjacentHTML("beforeend", `${liRating}`);
-      const imagen = document.querySelector("#imagen");
-      imagen.innerHTML = "";
-      imagen.insertAdjacentHTML(
-        "beforeend",
-        `<img src="${infoPelicula.Poster}" alt="Foto">`
-      );
+
+      // if (infoPelicula.Response === "False") {
+      //   document.querySelector("ocultar").style.display = "none";
+      //   document.querySelector("principal").style.display = "none";
+      //   document.querySelector("error").style.display = "block";
+      // }
+
+      // if (infoPelicula.Response === "False") {
+      //   document.querySelector("#principal").style.display = "none";
+      //   document.querySelector("#divDerecha").style.display = "none";
+      //   document.querySelector("#error").style.display = "block";
+      // } else {
+      //   document.querySelector("#divDerecha").style.display = "block";
+      //   document.querySelector("error").style.display = "none";
+      // }
     });
 });
